de8b14c703e2027c40312ed3d2c0b318d63466ce
@artanberishaa <artaanberishaa@gmail.com>
2022-06-23 21:21:37 +0200

    Netflix Clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Row from './Components/Row/Row';
+import requests from './requests';
+import Banner from './Components/Banner/Banner';
+import Nav from './Components/NavBar/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App" >
+          <Nav />
+          <Banner />
+          <Row
+            title="Netflix Originals" 
+              fetchUrl={requests.fetchNetflixOriginals}
+               isLargeRow = {true} />
+          <Row title="Top Rated" fetchUrl={requests.fetchTrending} />
+          <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+          <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+          <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+          <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+          <Row title="Documentaries" fetchUrl={requests.fetchDocumantaries} />
+         
     </div>
   );
 }
